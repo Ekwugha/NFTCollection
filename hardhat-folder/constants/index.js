52d36b5b1e462c7fcad4af9990145ffefc569227
Ekwugha <elloekwugha@gmail.com>
2022-05-12 21:27:50 +0100

    changed network from rinkeby to goerli

--- hardhat-folder/constants/index.js
+++ hardhat-folder/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x6bAAa32119f8661cd18FDD873d0E376d848b0F10";
+const WHITELIST_CONTRACT_ADDRESS = "0x27BfB89BCB9830bcb1fcEbcEd2f9733fEdAb6d1e";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-alpha-one.vercel.app/api/";
 
